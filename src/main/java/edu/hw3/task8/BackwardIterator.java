1179f2a8d91112430107cf7723e31fc999760f6b
cyberpanncake <cloudy9402@gmail.com>
2023-11-15 00:40:21 +0300

    fix: checkstyle and warnings

--- src/main/java/edu/hw3/task8/BackwardIterator.java
+++ src/main/java/edu/hw3/task8/BackwardIterator.java
@@ -3,13 +3,12 @@ package edu.hw3.task8;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
-import java.util.List;
 
 public class BackwardIterator<E> implements Iterator<E> {
-    private final List<E> collection;
+    private final ArrayList<E> collection;
     private int cursor;
 
-    public BackwardIterator(List<E> collection) {
+    public BackwardIterator(Collection<E> collection) {
         if (collection == null) {
             throw new IllegalArgumentException("Коллекция не должна быть пустой");
         }
@@ -25,6 +24,6 @@ public class BackwardIterator<E> implements Iterator<E> {
     @Override
     public E next() {
         cursor--;
-        return (E) collection.get(cursor);
+        return collection.get(cursor);
     }
 }
