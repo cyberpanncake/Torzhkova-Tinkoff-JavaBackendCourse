6f0a3e71349ae312ff59cd2e3607ed10f1c0050d
cyberpanncake <cloudy9402@gmail.com>
2023-12-03 14:55:16 +0300

    fix: warning

--- src/main/java/edu/hw8/task3/AbstractPasswordCracker.java
+++ src/main/java/edu/hw8/task3/AbstractPasswordCracker.java
@@ -33,8 +33,8 @@ public abstract class AbstractPasswordCracker {
             md.update(password.getBytes());
             byte[] bytes = md.digest();
             StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < bytes.length; i++) {
-                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            for (byte aByte : bytes) {
+                sb.append(Integer.toString((aByte & 0xff) + 0x100, 16).substring(1));
             }
             return sb.toString();
         } catch (NoSuchAlgorithmException e) {
