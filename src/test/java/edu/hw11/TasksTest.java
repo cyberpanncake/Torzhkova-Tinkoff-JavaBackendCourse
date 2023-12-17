48c3b414301fa0d4bb70c7996c029e2c780323c6
cyberpanncake <cloudy9402@gmail.com>
2023-12-18 00:27:34 +0300

    feat: task2 test

--- src/test/java/edu/hw11/TasksTest.java
+++ src/test/java/edu/hw11/TasksTest.java
@@ -1,10 +1,16 @@
 package edu.hw11;
 
 import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.agent.ByteBuddyAgent;
+import net.bytebuddy.dynamic.DynamicType;
 import net.bytebuddy.dynamic.loading.ClassLoadingStrategy;
+import net.bytebuddy.dynamic.loading.ClassReloadingStrategy;
 import net.bytebuddy.implementation.FixedValue;
+import net.bytebuddy.implementation.MethodDelegation;
+import net.bytebuddy.matcher.ElementMatchers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import java.io.IOException;
 import static net.bytebuddy.matcher.ElementMatchers.named;
 
 public class TasksTest {
@@ -20,5 +26,32 @@ public class TasksTest {
             .getLoaded();
         String actual = dynamicType.getDeclaredConstructor().newInstance().toString();
         Assertions.assertEquals(expected, actual);
+    }
+
+    static class ArithmeticUtils {
+        public static int sum(int a, int b) {
+            return a + b;
+        }
+    }
+
+    public static class NewUtils {
+        public static int sum(int a, int b) {
+            return a * b;
+        }
+    }
+
+    @Test
+    void task2Test() {
+        ByteBuddyAgent.install();
+        ClassReloadingStrategy classReloadingStrategy = ClassReloadingStrategy
+            .fromInstalledAgent();
+        DynamicType.Unloaded<ArithmeticUtils> unloaded = new ByteBuddy()
+            .redefine(ArithmeticUtils.class)
+            .method(ElementMatchers.named("sum"))
+            .intercept(MethodDelegation.to(NewUtils.class))
+            .make();
+        Assertions.assertEquals(6, ArithmeticUtils.sum(2, 4));
+        unloaded.load(ArithmeticUtils.class.getClassLoader(), classReloadingStrategy);
+        Assertions.assertEquals(8, ArithmeticUtils.sum(2, 4));
     }
 }
